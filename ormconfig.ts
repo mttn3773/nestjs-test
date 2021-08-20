d843d95ec15e5435464da743ee57a72206d09e8f
mttn373 <mttn3773@gmail.com>
2021-08-21 01:00:57 +0400

    Procfile

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,22 +1,44 @@
 const __prod__ = () => !!(process.env.NODE_ENV === 'production');
 const __test__ = () => !!(process.env.NODE_ENV === 'test');
+const pgString = require('pg-connection-string');
 
-const config = {
-  type: 'postgres',
-  host: process.env.POSTGRES_HOST,
-  port: parseInt(process.env.POSTGRES_PORT),
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: !__test__()
-    ? process.env.POSTGRES_DATABASE
-    : process.env.POSTGRES_TEST_DATABASE,
-  logging: true,
-  synchronize: true,
-  entities: ['dist/src/**/models/*.entity.js'],
-  migrations: ['dist/src/migrations/*.js'],
-  cli: {
-    migrationsDir: 'src/migrations',
-  },
+const databaseUrl = process.env.DATABASE_URL || '';
+const connectionOptions = pgString.parse(databaseUrl);
+const config = () => {
+  switch (process.env.NODE_ENV) {
+    case 'production':
+      return {
+        type: 'postgres',
+        host: connectionOptions.host,
+        port: parseInt(connectionOptions.port),
+        username: connectionOptions.user,
+        password: connectionOptions.password,
+        database: connectionOptions.database,
+        logging: true,
+        synchronize: false,
+        entities: ['dist/src/**/models/*.entity.js'],
+        migrations: ['dist/src/migrations/*.js'],
+        cli: {
+          migrationsDir: 'src/migrations',
+        },
+      };
+    default:
+      return {
+        type: 'postgres',
+        host: process.env.POSTGRES_HOST,
+        port: parseInt(process.env.POSTGRES_PORT),
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DATABASE,
+        logging: true,
+        synchronize: false,
+        entities: ['dist/src/**/models/*.entity.js'],
+        migrations: ['dist/src/migrations/*.js'],
+        cli: {
+          migrationsDir: 'src/migrations',
+        },
+      };
+  }
 };
 
-module.exports = config;
+module.exports = config();
